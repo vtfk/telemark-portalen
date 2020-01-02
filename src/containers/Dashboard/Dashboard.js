9c4441b78add7019e9500d3efb35772777779b73
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-01-02 16:47:14 +0100

    Updates frontpage. Removes unused items (patch)

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,41 +1,42 @@
 import React from 'react'
-import { Grid, Box } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
-
+import { Typography, Box } from '@material-ui/core'
 import PageWrapper from '../../components/PageWrapper'
 import Shortcuts from './Shortcuts'
-import News from './News'
-import Tasks from './Tasks'
-import Messages from './Messages'
-
-const useStyles = makeStyles(theme => ({
-  grid: {
-    [theme.breakpoints.up('sm')]: {
-      // flexDirection: 'row-reverse'
-    }
-  }
-}))
 
 const Dashboard = () => {
-  const classes = useStyles()
   return (
     <PageWrapper title="Forsiden">
       <Box mb={1}>
         <Shortcuts />
       </Box>
-      <Grid container spacing={2} className={classes.grid}>
-        <Grid item xs={12} sm={6}>
-          <Box mb={2}>
-            <Tasks />
-          </Box>
-          <Box>
-            <Messages />
-          </Box>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <News />
-        </Grid>
-      </Grid>
+      <Box>
+        <Typography variant="subtitle2" align="center">
+          Her finner du innganger til gamle fagsystemer eller systemer som ikke
+          er oppdatert etter sammenslåingen.
+          <br />
+          For alt annet bruker du{' '}
+          <strong>
+            <a
+              href="https://teams.microsoft.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              teams
+            </a>
+          </strong>{' '}
+          og{' '}
+          <strong>
+            <a
+              href="https://portalen.vfk.no/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              felles ansattportal
+            </a>
+          </strong>
+          .
+        </Typography>
+      </Box>
     </PageWrapper>
   )
 }
