684fb13b68a4e89a86912ef26687de1e7f4a4d8c
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-03-28 08:06:35 +0100

    Updates to latest uuid (patch)

--- api/auth/index.js
+++ api/auth/index.js
@@ -1,5 +1,5 @@
+import { v4 as uuid } from 'uuid'
 const { stringify } = require('querystring')
-const uuid = require('uuid/v4')
 const cookie = require('cookie')
 
 const { loadConfig } = require('../lib/authConfig')
@@ -10,11 +10,11 @@ module.exports = async (req, res) => {
     const nonce = uuid()
     res.setHeader('Set-Cookie', [
       cookie.serialize('baseDomain', baseDomain),
-      cookie.serialize('nonce', nonce)
+      cookie.serialize('nonce', nonce),
     ])
     res.json({
       baseDomain,
-      nonce
+      nonce,
     })
   } else {
     const { baseDomain, nonce } = req.cookies
@@ -23,7 +23,7 @@ module.exports = async (req, res) => {
       ...auth,
       nonce,
       state: nonce,
-      redirect_uri: `${baseDomain}/api/callback`
+      redirect_uri: `${baseDomain}/api/callback`,
     }
     res.status(301)
     res.setHeader(
